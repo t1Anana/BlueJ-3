6dc9e01835ad57e6674f0074f6514e38711f6902
Anto <ananagiphy@gmail.com>
2025-05-06 21:30:35 -0300

    Ejercicio 3.57

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -16,6 +16,7 @@ public class ClockDisplay
 {
     private NumberDisplay hours;
     private NumberDisplay minutes;
+    private NumberDisplay seconds;
     private String displayString;    // simulates the actual display
     
     /**
@@ -26,6 +27,7 @@ public class ClockDisplay
     {
         hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
+        seconds = new NumberDisplay(60);
         updateDisplay();
     }
 
@@ -34,11 +36,12 @@ public class ClockDisplay
      * creates a new clock set at the time specified by the 
      * parameters.
      */
-    public ClockDisplay(int hour, int minute)
+    public ClockDisplay(int hour, int minute, int second)
     {
         hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
-        setTime(hour, minute);
+        seconds = new NumberDisplay(60);
+        setTime(hour, minute, second);
     }
 
     /**
@@ -61,10 +64,11 @@ public class ClockDisplay
      * Set the time of the display to the specified hour and
      * minute.
      */
-    public void setTime(int hour, int minute)
+    public void setTime(int hour, int minute, int second)
     {
         hours.setValue(hour);
         minutes.setValue(minute);
+        seconds.setValue(second);
         updateDisplay();
     }
 
@@ -82,6 +86,7 @@ public class ClockDisplay
     private void updateDisplay()
     {
         displayString = hours.getDisplayValue() + ":" + 
-                        minutes.getDisplayValue();
+                        minutes.getDisplayValue() + ":" +
+                        seconds.getDisplayValue();
     }
 }
